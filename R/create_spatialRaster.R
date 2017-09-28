a0a77a543b235215c83ddfd42c487b06289956cb
Stevan Earl <stevan.earl@asu.edu>
2017-09-27 17:09:08 -0700

    update pkg details to file based approach

--- R/create_spatialRaster.R
+++ R/create_spatialRaster.R
@@ -13,23 +13,23 @@
 #' @note create_spatialRaster will look for a project id in the working
 #'   environment; this parameter is not passed to the function and it must
 #'   exist.
-#' @note create_spatialRaster currently requires an address to the file for
-#'   access from a local data catalog; if this parameter is missing, the
-#'   function defaults to the CAP LTER's file storage location.
+#' @note create_spatialRaster currently does not accept an argument for url to a
+#'   data file; the package defaults to a CAP LTER specific URL, and this must
+#'   be changed manually in the resulting xml
 #' @note create_spatialRaster relies on the helper functions zipRelatedFiles and
 #'   get_emlProjection, which are specific to UNIX operating systems.
 #'
-#' @param rasterName the quoted name of the raster file
-#' @param rasterValueAttrs raster attribute and details
-#' @param rasterValueFactors raster attribute details if attribute is of type
-#'   enumerated domain
-#' @param description quoted description of raster
-#' @param onlineURL quoted address of data file when accessed through a local
-#'   data catalog
+#' @param pathToRaster the quoted path of the raster directory
+#' @param metadataFile the quoted path and name of the raster metadata file
+#' @param categoricalMetadataFile quoted path and name of the raster metadata
+#'   file
 #'
 #' @import EML
-#' @importFrom raster raster bandnr
+#' @import dplyr
+#' @importFrom raster raster bandnr xres yres
+#' @importFrom readr read_csv
 #' @importFrom tools md5sum file_ext
+#' @importFrom stringr str_extract
 #'
 #' @return EML spatial data object is returned. Additionally, the spatial data
 #'   file is renamed with the project id + base file name + md5sum + file
@@ -37,82 +37,57 @@
 #'
 #' @examples
 #' \dontrun{
-#' The current workflow includes hand-coding metadata given that there will
-#' only ever be one attribute for a raster.
+#' The current workflow includes harvesting metadata from template files.
 #'
-#' Create attribute table manually, here calling the attribute 'value':
-#' rasterValueAttribute <-
-#'   data.frame(
-#'     attributeName = c('value'),
-#'     attributeLabel = c('categorical raster cell value'),
-#'     attributeDefinition = c('categorical value...land use/land cover type'),
-#'     definition = c('categorical value...land use/land cover type')
-#'   )
+#'  spatial_entity <- create_spatialRaster(
+#'    pathToRaster = '~/folder/',
+#'    metadataFile = ~/folder/CAP1985_metadata.csv',
+#'    categoricalMetadataFile = '~/folder/CAP1985_factors.csv'
+#'  )
 #'
-#' In this example, the raster value is of type enumerated domain so factor
-#' details must be provided.
-#'
-#' Generate factor details, here details are pulled from a file where the
-#' code and definition are detailed.
-#' rasterValueFactors <-
-#'   data.frame(
-#'     attributeName = "value",
-#'     code = rasterValuesMetadata$`Class ID`,
-#'     definition = rasterValuesMetadata$`Class Name`
-#'   )
-#'
-#'
-#' spatial_entity <- create_spatialRaster(rasterName = 'CAP_1985.img',
-#'                                        rasterValueAttrs = rasterValueAttribute,
-#'                                        rasterValueFactors = rasterValueFactors,
-#'                                        description = 'Phoenix-area raster',
-#'                                        onlineURL = 'https://data.gios.asu.edu/datasets/cap/'
-#'                                        )
 #' }
 #'
 #' @export
 
 create_spatialRaster <- function(pathToRaster, metadataFile, categoricalMetadataFile) {
 
-  
+
   # check for required environmental parameters and arguments
-  
+
     # do not proceed if the project id has not been identified in the working env
     if (!exists('projectid')) { stop("missing project id") }
-    
+
     # do not proceed if the path to where raster data reside is not provided
     if (!exists('pathToRaster')) { stop("specify the path to directory with raster data") }
-    
+
     # do not proceed if a metadata file is not provided
     if (!exists('metadataFile')) { stop("specify the raster metadata file") }
-  
-  
-  # use full path - UNIX specific!
+
+
+  # use full path
   pathToRaster <- path.expand(pathToRaster)
-  
+
   # load metadata file
   rasterMetadata <- read_csv(metadataFile)
-  
+
   # acces name of raster file
-  rasterFileName <- rasterMetadata %>% 
-    filter(metadata_entity == 'rasterName') %>% 
-    select(metadata_value) %>% 
+  rasterFileName <- rasterMetadata %>%
+    filter(metadata_entity == 'rasterName') %>%
+    select(metadata_value) %>%
     unlist(., use.names = FALSE)
-    
+
   # do not proceed if the raster file is not in the prescribed directory
   if(!file.exists(paste0(pathToRaster, "/", basename(rasterFileName)))) { stop("raster file is not in the prescribed directory") }
-  
+
   # identify raster location and file
   targetRaster <- paste0(pathToRaster, "/", rasterFileName)
- 
-  
+
+
   # load the raster file
-  # rasterObject <- raster(paste0(pathToRaster, "/", rasterFileName))
   rasterObject <- raster(targetRaster)
-  
+
 
   # read raster to access inherent file metadata
-  # rasterObject <- raster(rasterName)
   numBand <- new('numberOfBands',
                  bandnr(rasterObject))
   numRows <- new('rows',
@@ -150,8 +125,8 @@ create_spatialRaster <- function(pathToRaster, metadataFile, categoricalMetadata
 
   # generate path to newly created object
   newObjectLocation <- paste0(pathToRaster, objectName)
-  
-  
+
+
   # EML: physical
 
   # create @physical
@@ -194,89 +169,89 @@ create_spatialRaster <- function(pathToRaster, metadataFile, categoricalMetadata
     physical@dataFormat <- dat_format
   }
 
-  
+
   # generate raster attribute table
   rasterValueAttrs <- data.frame(
     attributeName = c('value'),
     attributeLabel = c(rasterMetadata %>%
-                         filter(metadata_entity == 'rasterValueLabel') %>% 
-                         select(metadata_value) %>% 
+                         filter(metadata_entity == 'rasterValueLabel') %>%
+                         select(metadata_value) %>%
                          unlist(., use.names = FALSE)),
     attributeDefinition = c(rasterMetadata %>%
-                              filter(metadata_entity == 'rasterValueDescription') %>% 
-                              select(metadata_value) %>% 
+                              filter(metadata_entity == 'rasterValueDescription') %>%
+                              select(metadata_value) %>%
                               unlist(., use.names = FALSE)),
     definition = c(rasterMetadata %>%
-                     filter(metadata_entity == 'rasterValueDescription') %>% 
-                     select(metadata_value) %>% 
+                     filter(metadata_entity == 'rasterValueDescription') %>%
+                     select(metadata_value) %>%
                      unlist(., use.names = FALSE))
   )
-  
-  
+
+
   # compile components for @attributeList of @dataTable
   # ignore factors if they are not relevant to this dataset
   if(missing(categoricalMetadataFile)) {
-    
+
     # determine raster value data type when not categorical
     nonFactor = case_when(
       is.numeric(getValues(rasterObject)) == TRUE ~ "numeric",
       is.character(getValues(rasterObject)) == TRUE ~ "character"
     )
-    
+
     attr_list <- set_attributes(attributes = rasterValueAttrs,
                                 col_classes = c(nonFactor))
-    
+
   } else {
-    
+
     # import categorical metadata
     rasterValueCategories <- read_csv(categoricalMetadataFile)
-    
+
     rasterValueFactors <- data.frame(
       attributeName = "value",
       code = rasterValueCategories$rasterValue,
       definition = rasterValueCategories$categoryName
     )
-    
+
     attr_list <- set_attributes(attributes = rasterValueAttrs,
                                 factors = rasterValueFactors,
                                 col_classes = c("factor"))
   }
-  
+
 
   # create spatialRaster
   newSR <- new("spatialRaster",
                entityName = objectName,
                entityDescription = rasterMetadata %>%
-                 filter(metadata_entity == 'rasterDescription') %>% 
-                 select(metadata_value) %>% 
+                 filter(metadata_entity == 'rasterDescription') %>%
+                 select(metadata_value) %>%
                  unlist(., use.names = FALSE),
                physical = physical,
                attributeList = attr_list,
                spatialReference = rasterProjection,
                horizontalAccuracy = rasterMetadata %>%
-                 filter(metadata_entity == 'horizontalAccuracy') %>% 
-                 select(metadata_value) %>% 
+                 filter(metadata_entity == 'horizontalAccuracy') %>%
+                 select(metadata_value) %>%
                  unlist(., use.names = FALSE),
                verticalAccuracy = rasterMetadata %>%
-                 filter(metadata_entity == 'verticalAccuracy') %>% 
-                 select(metadata_value) %>% 
+                 filter(metadata_entity == 'verticalAccuracy') %>%
+                 select(metadata_value) %>%
                  unlist(., use.names = FALSE),
                cellSizeXDirection = cellsX,
                cellSizeYDirection = cellsY,
                numberOfBands = numBand,
                rasterOrigin = rasterMetadata %>%
-                 filter(metadata_entity == 'rasterOrigin') %>% 
-                 select(metadata_value) %>% 
+                 filter(metadata_entity == 'rasterOrigin') %>%
+                 select(metadata_value) %>%
                  unlist(., use.names = FALSE),
                rows = numRows,
                columns = numCols,
                verticals = rasterMetadata %>%
-                 filter(metadata_entity == 'verticals') %>% 
-                 select(metadata_value) %>% 
+                 filter(metadata_entity == 'verticals') %>%
+                 select(metadata_value) %>%
                  unlist(., use.names = FALSE),
                cellGeometry = rasterMetadata %>%
-                 filter(metadata_entity == 'cellGeometry') %>% 
-                 select(metadata_value) %>% 
+                 filter(metadata_entity == 'cellGeometry') %>%
+                 select(metadata_value) %>%
                  unlist(., use.names = FALSE),
                id = objectName
   )
--- R/zipRelatedFiles.R
+++ R/zipRelatedFiles.R
@@ -16,11 +16,12 @@
 #'   extension (zip). Though intended primarily as a helper function,
 #'   zipRelatedFiles can be run independently.
 #'
-#' @note The target file must be in the current working directory.
 #' @note zipRelatedFiles is specific to UNIX operating environments.
 #'
-#' @param targetFile quoted name of target data file in the current working
-#'   directory
+#' @param pathToFile quoted name of the directory where the target file is
+#'   located directory
+#' @param targetFile quoted name of target data file to which other file names
+#'   in the prescribed directory will be evalated directory
 #'
 #' @importFrom utils zip
 #' @importFrom stringr str_extract
@@ -30,7 +31,14 @@
 #'   relevant when run independently
 #'
 #' @examples
-#' \dontrun{ zipRelatedFiles('CAP_1985.img') }
+#' \dontrun{
+#'
+#' zipRelatedFiles(
+#'   pathToFile = "~/folder/",
+#'   targetFile = "CAP_1985.img
+#' )
+#'
+#' }
 #'
 #' @export
 
@@ -38,28 +46,28 @@ zipRelatedFiles <- function(pathToFile, targetFile) {
 
   # check for requisite environment variables, function arguments, and that the target file exists in the
   # specified location
-  
+
     # do not proceed if the project id has not been identified in the working env
     if (!exists('projectid')) { stop("missing project id") }
-  
+
     # do not proceed if the path to where raster data reside is not provided
     if (!exists('pathToFile')) { stop("specify the path to directory with raster data") }
-    
+
     # do not proceed if a target file is not provided
     if (!exists('targetFile')) { stop("specify the target file") }
-    
+
     # do not proceed if the target file is not in the prescribed directory
     if(!file.exists(paste0(pathToFile, "/", basename(targetFile)))) { stop("target file is not in the prescribed directory") }
 
-  
+
   # use full path - UNIX specific!
   pathToFile <- path.expand(pathToFile)
-  
-  
+
+
   # strip the raster name of its file extension
   targetFileBaseName <- str_extract(basename(targetFile), "^[^\\.]*")
 
-  
+
   # we will need a temporary directory, ensure sure one with the same name does
   # not already exist
   temporaryDirectoryLocation <- paste0(pathToFile, '/temporaryDirectory')
@@ -68,47 +76,44 @@ zipRelatedFiles <- function(pathToFile, targetFile) {
   # create a temporary directory IF a directory with the base name of the target
   # file does not already exist as we will use this name
   namedDirectory <- paste0(pathToFile, '/', targetFileBaseName)
-  
+
   if (dir.exists(namedDirectory)) {
     stop(paste0("a directory with the name '", namedDirectory, "' already exists"))
   } else {
     dir.create(temporaryDirectoryLocation)
   }
 
-  
+
   # copy all files with the same name as passed into the same-named directory
   lapply(list.files(path = pathToFile, pattern = targetFileBaseName), function(x) {
     file.copy(paste0(pathToFile, '/', x), temporaryDirectoryLocation)
   })
 
-  
+
   # zip the directory with the target files, and rename with project id and hash
-  
+
     # userDirectory <- getwd()
     # setwd(pathToFile)
-     
+
     # rename the temporary directory to the base name of the target file
-    file.rename(temporaryDirectoryLocation, namedDirectory) 
-    
+    file.rename(temporaryDirectoryLocation, namedDirectory)
+
     # zip the target directory
-    # zip(zipfile = namedDirectory,
-    # zip(zipfile = targetFileBaseName,
-    #     files = paste0(namedDirectory, '/')) 
     system(paste0("zip -jX ", namedDirectory, " ", namedDirectory, "/*"))
-    
-    # mimic the name of the created zip entity 
-    created_zip_name <- paste0(targetFileBaseName, '.zip') 
-    
-    # create a new zip name with project & hash 
-    new_zip_name <- paste0(projectid, "_", targetFileBaseName, "_", md5sum(paste0(pathToFile, '/', created_zip_name)), ".zip") 
-    
-    # rename the created zip entity with the new name 
+
+    # mimic the name of the created zip entity
+    created_zip_name <- paste0(targetFileBaseName, '.zip')
+
+    # create a new zip name with project & hash
+    new_zip_name <- paste0(projectid, "_", targetFileBaseName, "_", md5sum(paste0(pathToFile, '/', created_zip_name)), ".zip")
+
+    # rename the created zip entity with the new name
     file.rename(paste0(pathToFile, '/', created_zip_name), paste0(pathToFile, '/', new_zip_name))
-    
+
     # remove the unzipped directory
     unlink(namedDirectory, recursive = TRUE)
 
-    
+
   # return the new zip file name for us in createEML functions
   return(new_zip_name)
 
