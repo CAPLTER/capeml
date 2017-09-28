aee7db5e75ffee004424234044f05e6cb98d154c
StevanEarl <stevan.earl@asu.edu>
2017-09-28 16:39:32 -0700

    Revert "removed inst/doc from gitignore; tracking vignette html"

--- inst/doc/create_spatialRaster.R
+++
@@ -1,5 +0,0 @@
-## ---- eval=FALSE---------------------------------------------------------
-#  cap1985 <- create_spatialRaster('~/knb-lter-cap.650.1/CAP 30m Landsat Series Submit/',
-#                                  '~/knb-lter-cap.650.1/rasterMetadataFiles/CAP1985_metadata.csv',
-#                                  '~/knb-lter-cap.650.1/rasterMetadataFiles/landsat_factors.csv')
-
