7782e7d9202b33e57d7609189b939205313eba78
Stevan Earl <stevan.earl@asu.edu>
2018-05-12 13:31:47 -0700

    separated create_keyword into a separate file

--- R/create_keywordSet.R
+++ R/create_keywordSet.R
@@ -1,9 +1,9 @@
 #' @title create_keywordSet
 #'
-#' @description create_keywordSe generates a EML entity of type
-#'   keywordSet
+#' @description create_keywordSet generates a EML entity of type
+#'   keywordSet.
 #'
-#' @details a keywordSet entity is created from a single data file (.csv) with
+#' @details A keywordSet entity is created from a single data file (.csv) with
 #'   the fields: thesaurus, keyword, and type where type is an intended (but
 #'   optional) attribute for the keyword.
 #'
@@ -18,17 +18,12 @@
 #' @examples
 #' \dontrun{
 #'
-#' need to add...
+#' datasetKeywordSet <- create_keywordSet('keywordTest.csv')
 #'
-#' The workflow features harvesting metadata from template files. Most metadata
-#' are documented in a raster-level metadata file (e.g., CAP1985_metadata.csv
-#' below). If raster values are categorical, an addiitonal metadata file is
-#' required to document the categories (e.g., CAP1985_factors.csv below)
+#' The workflow harvests keyword metadata from a keyword file (.csv) with the
+#' columns: thesaurus, keyword, type where type is an optional attribute that
+#' will be associated with the keyword EML/XML element.
 #'
-#'  spatial_entity <- create_spatialRaster(
-#'    pathToRaster = "~/folder/",
-#'    metadataFile = "~/folder/CAP1985_metadata.csv",
-#'    categoricalMetadataFile = "~/folder/CAP1985_factors.csv")
 #' }
 #'
 #' @export
@@ -46,8 +41,8 @@ create_keywordSet <- function(keywordsFile) {
     keywordList <- list()
     for (word in unique(keywordsSubset$keyword)) {
 
-      keywordIdent <- generateKeyword(datasetKeyword = keywordsSubset[keywordsSubset$keyword == word,]$keyword,
-                                      theAttribute = keywordsSubset[keywordsSubset$keyword == word,]$type)
+      keywordIdent <- create_keyword(datasetKeyword = keywordsSubset[keywordsSubset$keyword == word,]$keyword,
+                                     theAttribute = keywordsSubset[keywordsSubset$keyword == word,]$type)
       keywordList[[word]] <- keywordIdent
 
     } # close keywordList
@@ -63,26 +58,4 @@ create_keywordSet <- function(keywordsFile) {
 
   return(datasetKeywords)
 
-}
-
-#' @rdname create_keywordSet
-#'
-#' @description A helper function to generate EML entities of type keyword.
-#'
-#' @param datasetKeyword A keyword or subject.
-#' @param theAttribute A keyword attribute.
-#'
-#' @return EML keyword object is returned.
-#'
-#' @export
-#'
-generateKeyword <- function(datasetKeyword, theAttribute) {
-
-  emlKeyword <- as(datasetKeyword, "keyword")
-
-  if(!missing(theAttribute)) {
-    emlKeyword@keywordType <- as(theAttribute, "xml_attribute")
-  }
-
-  return(emlKeyword)
 }
