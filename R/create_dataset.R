e770d27876d3107488158d556f533da591a61999
Stevan Earl <stevan.earl@asu.edu>
2026-06-18 11:16:09 -0700

    develop: temporarily omitting remove annotation and custom_units files

--- R/create_dataset.R
+++ R/create_dataset.R
@@ -33,18 +33,18 @@ create_dataset <- function(
   ) {
 
   # pre-processing cleanup: remove stale unit/annotation YAMLs if present
-  if (file.exists("annotations.yaml")) {
-    try(file.remove("annotations.yaml"), silent = TRUE)
-  }
-  if (file.exists("custom_units.yaml")) {
-    try(file.remove("custom_units.yaml"), silent = TRUE)
-  }
-
-  # confirm required components exist in R environment
-
-  if (!exists("coverage")) {
-    stop("missing coverage")
-  }
+  # if (file.exists("annotations.yaml")) {
+  #   try(file.remove("annotations.yaml"), silent = TRUE)
+  # }
+  # if (file.exists("custom_units.yaml")) {
+  #   try(file.remove("custom_units.yaml"), silent = TRUE)
+  # }
+  #
+  # # confirm required components exist in R environment
+  #
+  # if (!exists("coverage")) {
+  #   stop("missing coverage")
+  # }
 
 
   # retrieve dataset details from config.yaml
